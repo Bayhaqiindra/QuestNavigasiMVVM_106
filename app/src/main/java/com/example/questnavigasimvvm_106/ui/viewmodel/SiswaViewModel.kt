6887393bf3e5b7dcd290b6c587e6b4c14c7f1f98
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:18:55 +0700

    menambahkan SiswaViewModel dengan MutableStateFlow untuk status UI siswa

--- app/src/main/java/com/example/questnavigasimvvm_106/ui/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/ui/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.questnavigasimvvm_106.ui.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.questnavigasimvvm_106.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SiswaViewModel : ViewModel() {
+    //request
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    //respons
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
 }
