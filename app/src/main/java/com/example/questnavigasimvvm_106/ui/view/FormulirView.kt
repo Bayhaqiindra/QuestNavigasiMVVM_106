58c85b3666c433414edd258d98e76fe86ab67098
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 19:58:33 +0700

    menambahkan state untuk input data formulir pada FormulirView

--- app/src/main/java/com/example/questnavigasimvvm_106/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/ui/view/FormulirView.kt
@@ -1,2 +1,24 @@
 package com.example.questnavigasimvvm_106.ui.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK : List<String>,
+    onSumbitClicked: (MutableList<String>) -> Unit
+){
+    var nama by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var notelepon by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nama, nim, gender, email, alamat, notelepon)
+}
