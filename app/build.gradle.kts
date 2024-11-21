4b58327fda38b78352ff8f1c80d5387fe9072091
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:32:15 +0700

    menambahkan NavigationControl dengan NavHost untuk navigasi antar halaman. lalu menambahkan buildgradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +67,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    //navigasi
+    implementation("androidx.navigation:navigation-compose:2.8.3")
 }
--- app/src/main/java/com/example/questnavigasimvvm_106/NavigationControl.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/NavigationControl.kt
@@ -1,6 +1,49 @@
 package com.example.questnavigasimvvm_106
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.example.questnavigasimvvm_106.ui.viewmodel.SiswaViewModel
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Halaman {
     FORMULIR,
     TAMPILDATA
+}
+
+@Composable
+fun NavigationControl(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+    Scaffold { isipadding ->
+        val uiState by viewModel.statusUI.collectAsState()
+        NavHost(
+            modifier = modifier
+                .padding(isipadding)
+                .fillMaxSize(),
+            navController = navHost,
+            startDestination = Halaman.FORMULIR.name
+        ) {
+
+        }
+    }
 }
