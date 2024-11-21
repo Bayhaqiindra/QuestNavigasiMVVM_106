d0482d2ae0654d08af0d334ce0f8a2415edb1b9e
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:02:06 +0700

    menambahkan layout dan pengaturan form dalam FormulirView dengan Column dan padding

--- app/src/main/java/com/example/questnavigasimvvm_106/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/ui/view/FormulirView.kt
@@ -1,11 +1,16 @@
 package com.example.questnavigasimvvm_106.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirView(
@@ -21,4 +26,12 @@ fun FormulirView(
     var gender by remember { mutableStateOf("") }
 
     val listData: MutableList<String> = mutableListOf(nama, nim, gender, email, alamat, notelepon)
+
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
 }
