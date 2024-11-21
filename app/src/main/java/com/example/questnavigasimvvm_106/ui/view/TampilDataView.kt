04688f6a8c046b5f420e258bed31b9c2b7643d83
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:06:33 +0700

    menambahkan composable TampilDataView dengan layout dasar dan parameter onBackButton

--- app/src/main/java/com/example/questnavigasimvvm_106/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/ui/view/TampilDataView.kt
@@ -1,2 +1,24 @@
 package com.example.questnavigasimvvm_106.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TampilDataView(
+    uiState: DataSiswa,
+    onBackButton: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
