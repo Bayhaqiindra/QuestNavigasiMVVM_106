1b7899dfd62ef48bdcd8a8270529099e9319677c
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:07:52 +0700

    menambahkan tampilan data siswa dengan komponen TampilData dan tombol Kembali

--- app/src/main/java/com/example/questnavigasimvvm_106/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/ui/view/TampilDataView.kt
@@ -3,6 +3,8 @@ package com.example.questnavigasimvvm_106.ui.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +21,14 @@ fun TampilDataView(
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        TampilData(param = "Nama", argu = uiState.nama)
+        TampilData(param = "NIM", argu = uiState.nim)
+        TampilData(param = "Jenis Kelamin", argu = uiState.gender)
+        TampilData(param = "Email", argu = uiState.email)
+        TampilData(param = "Alamat", argu = uiState.alamat)
+        TampilData(param = "No Telpon", argu = uiState.notelepon)
+        Button(onClick = onBackButton){
+            Text(text="Kembali")
+        }
     }
 }
