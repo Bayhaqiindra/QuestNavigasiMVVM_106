a2a806015cf58f04d46ad5069500eb823bc24767
Bayhaqiindra <bayhaqi30072004.com>
2024-11-21 20:15:30 +0700

    menambahkan daftar JenisKelamin dengan nilai laki-laki dan wanita

--- app/src/main/java/com/example/questnavigasimvvm_106/model/JenisKelamin.kt
+++ app/src/main/java/com/example/questnavigasimvvm_106/model/JenisKelamin.kt
@@ -1,4 +1,10 @@
 package com.example.questnavigasimvvm_106.model
 
+import com.example.questnavigasimvvm_106.R
+
 object JenisKelamin {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
